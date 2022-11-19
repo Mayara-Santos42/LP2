6e83e01843da8740ebe02dbdb941c835eafdc8bf
Mayara Santos <98860380+Mayara-Santos42@users.noreply.github.com>
2022-11-18 22:01:33 -0300

    Add files via upload

---
+++ PROVA/RA0030482123009/RA0030482123009/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RA0030482123009
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] Valores = new double[9 ,4];
+            double[] totalMeses = new double[9];
+            string valor;
+            double totalGeral = 0;
+            
+
+
+        }
+    }
+}
