a4061f05ae5d63f0e5e2916b67d2fd54a73a4ca3
Mayara Santos <98860380+Mayara-Santos42@users.noreply.github.com>
2023-05-26 20:36:30 -0300

    Add files via upload

---
+++ Prova/frmPrncipal.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Prova
+{
+    partial class frmPrncipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstbxVendas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(141, 102);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(185, 95);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lstbxVendas
+            // 
+            this.lstbxVendas.FormattingEnabled = true;
+            this.lstbxVendas.Location = new System.Drawing.Point(419, 27);
+            this.lstbxVendas.Name = "lstbxVendas";
+            this.lstbxVendas.Size = new System.Drawing.Size(169, 316);
+            this.lstbxVendas.TabIndex = 1;
+            // 
+            // frmPrncipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 360);
+            this.Controls.Add(this.lstbxVendas);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "frmPrncipal";
+            this.Text = "frmPrncipal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstbxVendas;
+    }
+}
---
+++ Prova/frmPrncipal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Prova
+{
+    public partial class frmPrncipal : Form
+    {
+        public frmPrncipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            double[,] Valores = new double[9, 4];
+            double[] TotalMes = new double[9];
+            string valor;
+            double totalGeral = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    valor = Interaction.InputBox("Digite o valor da semana: " + (j + 1) + "\n" +
+                        "Digite o valor do mês: " + (i + 1), "Entrada de Dados: ");
+                    if (valor == "")
+                    {
+                        MessageBox.Show("Valor vazio!!");
+                        j--;
+                    }
+                    else if (double.TryParse(valor, out Valores[i, j]))
+                    {
+                        double.TryParse(valor, out Valores[i, j]);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Digite apenas números!!");
+                        j--;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    TotalMes[i] = TotalMes[i] + Valores[i, j];
+                }
+                totalGeral = totalGeral + TotalMes[i];
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    lstbxVendas.Items.Add("Total do Mês: " + (i + 1) + " Semana: " + (j + 1) + ": R$" + Valores[i, j].ToString("N2"));
+                }
+                lstbxVendas.Items.Add(">>Total Mês: R$" + TotalMes[i].ToString("N2"));
+                lstbxVendas.Items.Add("...............................");
+            }
+            lstbxVendas.Items.Add("Total Geral: R$" + totalGeral.ToString("N2"));
+
+        }
+    }
+}
